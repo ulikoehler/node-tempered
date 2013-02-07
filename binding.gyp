c80bd2f9204024e9de274ac7ece934c7acb99e5d
Uli Köhler <ulikoehler@online.de>
2013-02-07 04:38:57 +0100

    Compilation without system-wide library works now

--- binding.gyp
+++ binding.gyp
@@ -2,9 +2,13 @@
     "targets": [{
         "target_name": "tempered",
         "sources": ["tempered.cpp"],
+	"include_dirs": ["./thirdparty/install/include"],
+	"library_dirs": ["./thirdparty/install/lib"],
 
         "link_settings": {
-            "libraries": ["-ltempered", "-lhidapi-hidraw"]
+	    'ldflags': ['-L../thirdparty/install/lib'],
+            "libraries": ["-ltempered","-lhidapi-hidraw"],
+	    "library_dirs": ["./thirdparty/install/lib"]
         }
     }]
 }
--- thirdparty/compile.sh
+++ thirdparty/compile.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 export WD=$(pwd)
+#Note: We have to compile the static libraries with -fPIC because NodeJS links them into a shared library
 #HIDAPI
 echo "Compiling HIDAPI..."
 cd hidapi
 ./bootstrap > /dev/null
-./configure --prefix=$WD/install --exec-prefix=$WD/install --enable-static --disable-shared > /dev/null
-make > /dev/null
+./configure CFLAGS=-fPIC CPPFLAGS=-fPIC --prefix=$WD/install --exec-prefix=$WD/install --enable-static --disable-shared > /dev/null
+make
 make install > /dev/null
 #Compile libtempered
 echo "Compiling libtempered..."
 cd ../tempered
-cmake -DBUILD_HIDAPI_SHARED=OFF -DHIDAPI_LINK_LIBS=udev -DBUILD_SHARED_LIB=OFF -DBUILD_STATIC_LIB=ON -DHIDAPI_LIB=$WD/install/lib/libhidapi-hidraw.a -DCMAKE_INSTALL_PREFIX:PATH=$WD/install/ .
+cmake -DCMAKE_C_FLAGS=-fPIC -DBUILD_HIDAPI_SHARED=OFF -DHIDAPI_LINK_LIBS=udev -DBUILD_SHARED_LIB=OFF -DBUILD_STATIC_LIB=ON -DHIDAPI_LIB=$WD/install/lib/libhidapi-hidraw.a -DCMAKE_INSTALL_PREFIX:PATH=$WD/install/ .
 make > /dev/null
 make install
