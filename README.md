206c6e3ad3b51e6f76b53e882f4beb64748c9397
Uli Köhler <ulikoehler@online.de>
2013-02-07 04:03:53 +0100

    Fix compiler script

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A Node.JS API for the TEMPered library from https://github.com/edorfaus/TEMPered
 
 h2. Installation
 
+* Install dependencies (Ubuntu 12.04+): sudo apt-get install build-essential libtool libusb-1.0-0-dev libudev-dev cmake
 * Compile and install HIDAPI as described on the TEMPered page
 * Compile and install libtempered. libtempered-util is not needed by this software
 * This program should work with a static libtempered library, however I didn't test that.
--- thirdparty/compile.sh
+++ thirdparty/compile.sh
@@ -4,12 +4,12 @@ export WD=$(pwd)
 echo "Compiling HIDAPI..."
 cd hidapi
 ./bootstrap > /dev/null
-./configure --prefix=$WD/../install --exec-prefix=$WD/../install --enable-static --disable-shared > /dev/null
+./configure --prefix=$WD/install --exec-prefix=$WD/install --enable-static --disable-shared > /dev/null
 make > /dev/null
 make install > /dev/null
 #Compile libtempered
 echo "Compiling libtempered..."
-cd ../libtempered
-cmake -DHIDAPI_LIB=$WD/install/lib/libhidapi-libusb.a -DCMAKE_INSTALL_PREFIX:PATH=$WD/install/ .
+cd ../tempered
+cmake -DBUILD_HIDAPI_SHARED=OFF -DHIDAPI_LINK_LIBS=udev -DBUILD_SHARED_LIB=OFF -DBUILD_STATIC_LIB=ON -DHIDAPI_LIB=$WD/install/lib/libhidapi-hidraw.a -DCMAKE_INSTALL_PREFIX:PATH=$WD/install/ .
 make > /dev/null
 make install
